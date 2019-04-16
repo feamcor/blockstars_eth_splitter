c15d450e0a9fea89e32c0049d2f0ed9b012af33c
Fabio Correa <feamcor@gmail.com>
2019-04-16 15:54:48 -0400

    Solidity test case to check behavior of contract accounts

--- test/TestSplitter.sol
+++ test/TestSplitter.sol
@@ -5,7 +5,9 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Splitter.sol";
 import "./helpers/Actor.sol";
 
-contract TestSplitter {    
+contract TestSplitter {  
+    uint public initialBalance = 1 ether;
+
     function testDeployed() public {
         Splitter _splitter = Splitter(DeployedAddresses.Splitter());
         Assert.balanceIsZero(address(_splitter), "initial balance is not zero");   
@@ -13,12 +15,24 @@ contract TestSplitter {
 
     function testContractAccounts() public {
         Actor _alice = new Actor();
+        Assert.balanceIsZero(address(_alice), "balance is not zero (alice)");
         Actor _bob = new Actor();
+        Assert.balanceIsZero(address(_bob), "balance is not zero (bob)");
         Actor _carol = new Actor();
-        uint _deposit = 1_000_000 wei;
+        Assert.balanceIsZero(address(_carol), "balance is not zero (carol)");
         Splitter _splitter = _alice.newSplitter(address(_bob), address(_carol));
-        Assert.balanceIsZero(address(_splitter), "initial balance is not zero");
-        _alice.deposit(_splitter, _deposit);
-        Assert.balanceEqual(address(_splitter), _deposit, "balance mismatch");
+        Assert.balanceIsZero(address(_splitter), "balance is not zero (splitter)");
+        address(_alice).transfer(5 szabo);
+        Assert.balanceEqual(address(_alice), 5 szabo, "transfer mismatch (alice)");
+        address(_bob).transfer(1 szabo);
+        Assert.balanceEqual(address(_bob), 1 szabo, "transfer mismatch (bob)");
+        address(_carol).transfer(1 szabo);
+        Assert.balanceEqual(address(_carol), 1 szabo, "transfer mismatch (carol)");
+        _alice.deposit(_splitter, 2 szabo);
+        Assert.balanceEqual(address(_splitter), 2 szabo, "deposit mismatch (splitter)");
+        _bob.withdraw(_splitter);
+        Assert.balanceEqual(address(_bob), 2 szabo, "withdraw mismatch (bob)");
+        _carol.withdraw(_splitter);
+        Assert.balanceEqual(address(_carol), 2 szabo, "withdraw mismatch (carol)");
     }
 }
--- test/helpers/Actor.sol
+++ test/helpers/Actor.sol
@@ -3,6 +3,9 @@ pragma solidity 0.5.2;
 import "../../contracts/Splitter.sol";
 
 contract Actor {
+
+    function() external payable {}
+
     function newSplitter(address _r1, address _r2) public returns(Splitter) {
         return new Splitter(_r1, _r2);
     }
@@ -10,7 +13,7 @@ contract Actor {
     function deposit(Splitter _splitter, uint _amount) public {
         require(address(_splitter) != address(0x0), "splitter is undefined");
         require(_amount != uint(0), "deposit amount is zero");
-        _splitter.deposit();
+        _splitter.deposit.value(_amount)();
     }
 
     function withdraw(Splitter _splitter) public {
