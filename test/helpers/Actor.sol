ee1368f5e69f6c171ca64648aac2627f496d1844
Fabio Correa <feamcor@gmail.com>
2019-04-18 16:02:17 -0400

    Added events to view sequence of execution

--- test/helpers/Actor.sol
+++ test/helpers/Actor.sol
@@ -3,21 +3,28 @@ pragma solidity 0.5.2;
 import "../../contracts/Splitter.sol";
 
 contract Actor {
+    event SplitterInstantiated(address indexed by, address splitter, address indexed recipient1, address indexed recipient2);
+    event SplitFired(address indexed by, uint amount);
+    event WithdrawFired(address indexed by);
 
     function() external payable {}
 
     function newSplitter(address _r1, address _r2) public returns(Splitter) {
-        return new Splitter(_r1, _r2);
+        Splitter _splitter = new Splitter(_r1, _r2);
+        emit SplitterInstantiated(address(this), address(_splitter), _r1, _r2);
+        return _splitter;
     }
 
     function split(Splitter _splitter, uint _amount) public {
         require(address(_splitter) != address(0x0), "splitter is undefined");
         require(_amount != uint(0), "deposit amount is zero");
         _splitter.split.value(_amount)();
+        emit SplitFired(address(this), _amount);
     }
 
     function withdraw(Splitter _splitter) public {
         require(address(_splitter) != address(0x0), "splitter is undefined");
         _splitter.withdraw();
+        emit WithdrawFired(address(this));
     }
 }
