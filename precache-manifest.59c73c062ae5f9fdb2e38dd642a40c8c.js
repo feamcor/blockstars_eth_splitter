82c84aa6ea416f0d51420617453c0e4422611c61
Fabio Correa <feamcor@gmail.com>
2019-05-04 22:30:18 -0400

    Updates

---
+++ precache-manifest.59c73c062ae5f9fdb2e38dd642a40c8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blockstars_eth_splitter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "324239c322b1ab2ec39c",
+    "url": "/blockstars_eth_splitter/static/js/runtime~main.b43e643e.js"
+  },
+  {
+    "revision": "812b41b659d0498d49fd",
+    "url": "/blockstars_eth_splitter/static/js/main.bc2a9db4.chunk.js"
+  },
+  {
+    "revision": "6c67dfd29e33b857e2b0",
+    "url": "/blockstars_eth_splitter/static/js/2.24d0748d.chunk.js"
+  },
+  {
+    "revision": "6c67dfd29e33b857e2b0",
+    "url": "/blockstars_eth_splitter/static/css/2.4915e3e5.chunk.css"
+  },
+  {
+    "revision": "8ba1427258010ada7a1da820f59f7301",
+    "url": "/blockstars_eth_splitter/index.html"
+  }
+];
